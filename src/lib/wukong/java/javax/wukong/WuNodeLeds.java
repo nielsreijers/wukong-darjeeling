eaaa4f955b3c83133bf55fa3887036bdb8467af2
Niels Reijers <nielsreijers@gmail.com>
2013-01-06 14:19:45 +0800

    Changed WuNodeLeds naming to reflect darjeeling style. Moved impl. to arduino specific directory.

--- src/lib/wukong/java/javax/wukong/WuNodeLeds.java
+++ src/lib/wukong/java/javax/wukong/WuNodeLeds.java
@@ -1,16 +1,22 @@
 package javax.wukong;
 
-public class WuNodeLeds {
+public class WuNodeLeds
+{
 	public static final byte LED1 = 0;
 	public static final byte LED2 = 0;
 	public static final byte LED3 = 0;
 	public static final byte LED4 = 0;
 
-	static {
-		init();
+    public static native void _setLed(int led, boolean state);
+    private static native void _init();
+
+	static
+	{
+		_init();
 	}
 
-    public static native void setLed(int led, boolean state);
-
-    private static native void init();
+	public static void setLed(int led, boolean state)
+	{
+		_setLed(led, state);
+	}
 }
