db21950db46bfeea02bcd71767d6a553b71312e4
Niels Reijers <nielsreijers@gmail.com>
2013-01-12 22:04:07 +0800

    Fixed LED constants in WuNodeLeds class.

--- src/lib/wukong/java/javax/wukong/WuNodeLeds.java
+++ src/lib/wukong/java/javax/wukong/WuNodeLeds.java
@@ -3,9 +3,9 @@ package javax.wukong;
 public class WuNodeLeds
 {
 	public static final byte LED1 = 0;
-	public static final byte LED2 = 0;
-	public static final byte LED3 = 0;
-	public static final byte LED4 = 0;
+	public static final byte LED2 = 1;
+	public static final byte LED3 = 2;
+	public static final byte LED4 = 3;
 
     public static native void _setLed(int led, boolean state);
     private static native void _init();
