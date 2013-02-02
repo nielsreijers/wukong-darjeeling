5b5084f08f8d041bb843b2b7c04344af4ae014d3
Niels Reijers <nielsreijers@gmail.com>
2013-02-02 10:13:42 +0800

    Added empty lib for wukong comm layer, and modified ant script to copy a public header file so libraries can depend on other libraries in C.

---
+++ src/lib/wkcomm/c/common/wkcomm.c
@@ -0,0 +1,28 @@
+#include "panic.h"
+#include "wkcomm.h"
+
+
+// // Initialise wkcomm and whatever protocols are enabled.
+// void wkcomm_init(void) {
+// 	dj_panic(DJ_PANIC_UNIMPLEMENTED_FEATURE);
+// }
+
+// // Get my own node id
+// address_t wkcomm_get_node_id() {
+// 	dj_panic(DJ_PANIC_UNIMPLEMENTED_FEATURE);
+// }
+
+// // Call this periodically to receive data
+// void wkcomm_poll(void) {
+// 	dj_panic(DJ_PANIC_UNIMPLEMENTED_FEATURE);
+// }
+
+// // Send length bytes to dest
+// int wkcomm_send(address_t dest, uint8_t command, uint8_t *payload, uint8_t length) {
+// 	dj_panic(DJ_PANIC_UNIMPLEMENTED_FEATURE);
+// }
+
+// // Wait for a message of a specific type, while still handling messages of other types
+// wkcomm_message *wkcomm_wait(u16_t wait_msec, uint8_t *commands, uint8_t number_of_commands) {
+// 	dj_panic(DJ_PANIC_UNIMPLEMENTED_FEATURE);
+// }
---
+++ src/lib/wkcomm/c/common/wkcomm.h
@@ -0,0 +1,33 @@
+#ifndef WKCOMMH
+#define WKCOMMH
+
+#include "types.h"
+
+
+
+// Temporary file until I decide how to implement the communication layer properly.
+
+// WuKong address. For now it's just a byte, but this will probably change.
+// When it does, we need to change the component-node map as well
+typedef uint8_t address_t;
+
+#define nvmcomm_get_node_id()		((address_t)1)
+/// End temporary code
+
+
+// Initialise wkcomm and whatever protocols are enabled.
+extern void wkcomm_init(void);
+
+// Get my own node id
+extern address_t wkcomm_get_node_id();
+
+// Call this periodically to receive data
+extern void wkcomm_poll(void);
+
+// Send length bytes to dest
+extern int wkcomm_send(address_t dest, uint8_t command, uint8_t *payload, uint8_t length);
+
+// // Wait for a message of a specific type, while still handling messages of other types
+// extern wkcomm_message *wkcomm_wait(u16_t wait_msec, uint8_t *commands, uint8_t number_of_commands);
+
+#endif // WKCOMMH
