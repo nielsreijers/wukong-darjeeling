1b73bd7d75e8266c2988765ababb85959b5db46e
Niels Reijers <nielsreijers@gmail.com>
2013-02-09 15:59:14 +0800

    Added some code to trigger initialisation of wkcomm.

---
+++ src/lib/wkcomm/c/common/javax_wukong_wkcomm_WKComm.c
@@ -0,0 +1,5 @@
+#include "wkcomm.h"
+
+void javax_wukong_wkcomm_WKComm_void__init() {
+	wkcomm_init();
+}
---
+++ src/lib/wkcomm/java/javax/wukong/wkcomm/WKComm.java
@@ -0,0 +1,11 @@
+package javax.wukong.wkcomm;
+
+public class WKComm
+{
+    private static native void _init();
+
+	static
+	{
+		_init();
+	}
+}
