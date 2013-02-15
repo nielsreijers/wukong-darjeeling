cc28560d729ab9909c30d88d0efa74ff0139cd03
Niels Reijers <nielsreijers@gmail.com>
2013-02-15 19:23:16 +0800

    Fixes to java code generation.

--- wukong/master/wkpf/codegen.py
+++ wukong/master/wkpf/codegen.py
@@ -98,9 +98,10 @@ class CodeGen:
     # component xml dom
     if len(args) == 2:
       # Directories
-      self.global_vm_dir = os.path.join('vm', 'src')
-      self.vm_dir = os.path.join('vm', 'src', 'native_wuclasses')
-      self.java_dir = os.path.join('java', 'nanovm', 'wkpf')
+
+      self.global_vm_dir = os.path.join('src', 'lib', 'wkpf', 'c', 'common')
+      self.vm_dir = os.path.join('src', 'lib', 'wkpf', 'c', 'common', 'native_wuclasses')
+      self.java_dir = os.path.join('src', 'lib', 'wkpf', 'java', 'javax', 'wukong', 'wkpf', 'wuclasses')
 
       self.component_string = args[0]
       self.project_dir = args[1]
@@ -137,7 +138,7 @@ class CodeGen:
     app.info("==================Begin TypeDefs=====================")
     # Boilerplate for Java global constants file
     self.global_virtual_constants_lines.append('''
-        package nanovm.wkpf;
+        package javax.wukong.wkpf;
 
         public class GENERATEDWKPF {
     ''')
@@ -217,7 +218,9 @@ class CodeGen:
       # Parsing to WuKong Profile Framework Component Library header in Java
       if wuclass.getAttribute('virtual') == 'true':
         wuclass_virtual_super_lines.append('''
-        package nanovm.wkpf;
+        package javax.wukong.wkpf.wuclasses;
+        import javax.wukong.wkpf.VirtualWuObject;
+        import javax.wukong.wkpf.WKPF;
 
         public abstract class %s extends VirtualWuObject {
           public static byte[] properties = new byte[] {
