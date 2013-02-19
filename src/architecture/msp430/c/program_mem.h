aec79d86e3df3bd4bfff6f8bd36d146d083ec82e
Niels Reijers <nielsreijers@gmail.com>
2013-02-19 23:49:40 +0800

    Changed program_mem to just use a normal pointer deference. No idea if that works (but I think it should on the 48K taroko), but for now I'm just trying to get it to compile first.

--- src/architecture/msp430/c/program_mem.h
+++ src/architecture/msp430/c/program_mem.h
@@ -29,25 +29,9 @@ typedef void * dj_di_pointer;
 #define DJ_DI_NOT_SET (dj_di_pointer)(-1)
 #define DJ_DI_NOT_FOUND (dj_di_pointer)(-2)
 
-#if (PLATFORM==ocapi)
-
-#include "faraccess.h"
-#define dj_di_getU8(pointer)  xldb( (unsigned int)(pointer) )
-#define dj_di_getU16(pointer) xldb2( (unsigned int)(pointer) )
-#define dj_di_getU32(pointer) xldb4( (unsigned int)(pointer) )
-
-#else
-#error "No ocapi?!"
 #define dj_di_getU8(pointer)  (*(uint8_t*) (pointer))
-#define dj_di_getU16(pointer) ( ((*(uint8_t*) (pointer+1))<<8) | (*(uint8_t*) (pointer)) )
-#define dj_di_getU32(pointer) (\
-	((*(uint8_t*) (pointer+3))<<24) |\
-	((*(uint8_t*) (pointer+2))<<16) |\
-	((*(uint8_t*) (pointer+1))<<8) |\
-	(*(uint8_t*) (pointer)) )
-
-#endif
-
-#define dj_di_getLocalId(pointer) ((dj_local_id){dj_di_getU8(pointer),dj_di_getU8(pointer+1)})
+#define dj_di_getU16(pointer) (*(uint16_t*)(pointer))
+#define dj_di_getU32(pointer) (*(uint32_t*)(pointer))
+#define dj_di_getLocalId(pointer) (*(dj_local_id*)(pointer))
 
 #endif
