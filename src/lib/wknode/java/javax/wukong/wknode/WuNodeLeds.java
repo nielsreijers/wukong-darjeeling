e8891ddb323eb4a717d3eeab435a2dd6b33298e4
Niels Reijers <nielsreijers@gmail.com>
2013-03-18 17:00:29 +0800

    Small refactoring of wknode lib.

--- src/lib/wknode/java/javax/wukong/wknode/WuNodeLeds.java
+++ src/lib/wknode/java/javax/wukong/wknode/WuNodeLeds.java
@@ -7,16 +7,11 @@ public class WuNodeLeds
 	public static final byte LED3 = 2;
 	public static final byte LED4 = 3;
 
-    public static native void _setLed(int led, boolean state);
+    public static native void setLed(int led, boolean state);
     private static native void _init();
 
 	static
 	{
 		_init();
-	}
-
-	public static void setLed(int led, boolean state)
-	{
-		_setLed(led, state);
 	}
 }
