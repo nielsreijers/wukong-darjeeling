80933f741e3e66df3528b5c6c0ae167d687603c6
Niels Reijers <nielsreijers@gmail.com>
2013-03-22 13:43:50 +0800

    Changed some debug output from hex to decimal.

--- src/lib/wkpf/c/common/wkpf_wuclasses.c
+++ src/lib/wkpf/c/common/wkpf_wuclasses.c
@@ -10,13 +10,13 @@ wuclass_t *wuclasses_list = NULL;
 void wkpf_register_wuclass(wuclass_t *wuclass) {
   wuclass->next = wuclasses_list;
   wuclasses_list = wuclass;
-  DEBUG_LOG(DBG_WKPF, "WKPF: Registering wuclass id %x at index %x\n", wuclass->wuclass_id, wkpf_get_number_of_wuclasses());
+  DEBUG_LOG(DBG_WKPF, "WKPF: Registering wuclass id %d at index %d\n", wuclass->wuclass_id, wkpf_get_number_of_wuclasses());
 }
 
 uint8_t wkpf_register_virtual_wuclass(uint16_t wuclass_id, update_function_t update, uint8_t number_of_properties, uint8_t properties[]) {
 	wuclass_t *wuclass;
 	if (wkpf_get_wuclass_by_id(wuclass_id, &wuclass) != WKPF_ERR_WUCLASS_NOT_FOUND) {
-		DEBUG_LOG(DBG_WKPF, "WKPF: WuClass id in use while registering wuclass id %x: FAILED\n", wuclass->wuclass_id);
+		DEBUG_LOG(DBG_WKPF, "WKPF: WuClass id in use while registering wuclass id %d: FAILED\n", wuclass->wuclass_id);
 		return WKPF_ERR_WUCLASS_ID_IN_USE;
 	}
 
@@ -26,7 +26,7 @@ uint8_t wkpf_register_virtual_wuclass(uint16_t wuclass_id, update_function_t upd
   wuclass = (wuclass_t*)dj_mem_alloc(size, CHUNKID_WUCLASS);
   dj_mem_removeSafePointer((void**)&properties);
   if (wuclass == NULL) {
-  	DEBUG_LOG(DBG_WKPF, "WKPF: Out of memory while registering wuclass id %x: FAILED\n", wuclass->wuclass_id);
+  	DEBUG_LOG(DBG_WKPF, "WKPF: Out of memory while registering wuclass id %d: FAILED\n", wuclass->wuclass_id);
   	return WKPF_ERR_OUT_OF_MEMORY;
   }
 
@@ -50,7 +50,7 @@ uint8_t wkpf_get_wuclass_by_id(uint16_t wuclass_id, wuclass_t **wuclass) {
 			return WKPF_OK;
 		*wuclass = (*wuclass)->next;
 	}
-	DEBUG_LOG(DBG_WKPF, "WKPF: No wuclass with id %x found: FAILED\n", wuclass_id);
+	DEBUG_LOG(DBG_WKPF, "WKPF: No wuclass with id %d found: FAILED\n", wuclass_id);
 	return WKPF_ERR_WUCLASS_NOT_FOUND;
 }
 
