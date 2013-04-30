0f81c3c2b43b7f0dd3c10b9590eb049f859137e9
catlikethief <a50119729@gmail.com>
2013-04-30 11:08:36 +0800

    merge led

--- src/config/native/c/config.h
+++ src/config/native/c/config.h
@@ -36,21 +36,20 @@ typedef int64_t dj_time_t;
 // #define ALIGN_16
 
 /* Please see common/debug.h */
-// #define DARJEELING_DEBUG
+#define DARJEELING_DEBUG
 // #define DARJEELING_DEBUG_FRAME
 // #define DARJEELING_DEBUG_MEM_TRACE
 // #define DARJEELING_DEBUG_TRACE
 // #define DARJEELING_DEBUG_CHECK_HEAP_SANITY
 // #define DARJEELING_DEBUG_PERFILE
-// #define DBG_DARJEELING true
+#define DBG_DARJEELING true
 // #define DBG_DARJEELING_GC true
-// #define DBG_WKPF true
+#define DBG_WKPF true
 // #define DBG_WKPFGC true
 // #define DBG_WKPFUPDATE true
 #define DBG_WKCOMM true
 #define DBG_WKREPROG true
 #define DBG_ZWAVETRACE true
-
 #define DARJEELING_PRINTF printf
 
 #define DARJEELING_PGMSPACE_MACRO
