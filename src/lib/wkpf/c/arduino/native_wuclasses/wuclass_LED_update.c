4f2ab48b39382b8162d367032d8fc6a3f73c3ae7
catlikethief <a50119729@gmail.com>
2013-05-01 09:28:45 +0800

    In every update, toggle the LEDs for those default properties are set true in standard library, and remain off for those default properties are set false

--- src/lib/wkpf/c/arduino/native_wuclasses/wuclass_LED_update.c
+++ src/lib/wkpf/c/arduino/native_wuclasses/wuclass_LED_update.c
@@ -2,6 +2,7 @@
 #include "../../common/native_wuclasses/native_wuclasses.h"
 #include <avr/io.h>
 #include "../../../../vm/c/common/execution.h"
+#include "GENERATEDwuclass_led.h"
 
 
 #define output_low(port, pin) port &= ~(1<<pin)
@@ -12,22 +13,27 @@
 #ifdef ENABLE_WUCLASS_LED
 
 void wuclass_led_setup(wuobject_t *wuobject) {
-
-}
-
-void wuclass_led_update(wuobject_t *wuobject) {
-    bool p1, p2, p3, p4;
-    DEBUG_LOG(DBG_WKCOMM, "WKCOMMUPDATE: led\n");
-
     set_output(DDRK, 0);
     set_output(DDRK, 1);
     set_output(DDRK, 2);
     set_output(DDRK, 3);
+}
 
-    //output_low(PORTK, 0);
-    //output_low(PORTK, 1);
-    //output_low(PORTK, 2);
-    //output_low(PORTK, 3);
+void set_led(uint8_t pin, bool onOff) {
+    if (onOff)
+        output_low(PORTK, pin);
+    else
+        output_high(PORTK, pin);
+}
+
+void wuclass_led_update(wuobject_t *wuobject) {
+    bool p1, p2, p3, p4;
+
+    bool *onOff;
+    onOff = wuclass_led_getPrivateData(wuobject);
+    *onOff = !(*onOff);
+
+    DEBUG_LOG(DBG_WKPFUPDATE, "WKCOMMUPDATE: led %d\n", *onOff);
 
     wkpf_internal_read_property_boolean(wuobject, WKPF_PROPERTY_LED_PORT1, &p1);
     wkpf_internal_read_property_boolean(wuobject, WKPF_PROPERTY_LED_PORT2, &p2);
@@ -35,21 +41,21 @@ void wuclass_led_update(wuobject_t *wuobject) {
     wkpf_internal_read_property_boolean(wuobject, WKPF_PROPERTY_LED_PORT4, &p4);
 
     if(p1==true)
-        output_low(PORTK, 0);
+        set_led(0, *onOff);
     else
-        output_high(PORTK, 0);
+        set_led(0, false);
     if(p2==true)
-        output_low(PORTK, 1);
+        set_led(1, *onOff);
     else
-        output_high(PORTK, 1);
+        set_led(1, false);
     if(p3==true)
-        output_low(PORTK, 2);
+        set_led(2, *onOff);
     else
-        output_high(PORTK, 2);
+        set_led(2, false);
     if(p4==true)
-        output_low(PORTK, 3);
+        set_led(3, *onOff);
     else
-        output_high(PORTK, 3);
+        set_led(3, false);
 }
 
 #endif // ENABLE_WUCLASS_LED
