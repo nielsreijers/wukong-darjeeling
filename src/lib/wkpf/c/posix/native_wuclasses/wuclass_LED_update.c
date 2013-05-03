7fa22df3382a29b095a586fda49e28c1a9fd1c9f
Niels Reijers <nielsreijers@gmail.com>
2013-05-03 11:50:41 +0800

    Add empty update for LED wuclass on native platform

---
+++ src/lib/wkpf/c/posix/native_wuclasses/wuclass_LED_update.c
@@ -0,0 +1,14 @@
+#include "debug.h"
+#include "../../common/native_wuclasses/native_wuclasses.h"
+#include "../../../../vm/c/common/execution.h"
+#include "../../common/native_wuclasses/GENERATEDwuclass_led.h"
+
+#ifdef ENABLE_WUCLASS_LED
+
+void wuclass_led_setup(wuobject_t *wuobject) {
+}
+
+void wuclass_led_update(wuobject_t *wuobject) {
+}
+
+#endif // ENABLE_WUCLASS_LED
