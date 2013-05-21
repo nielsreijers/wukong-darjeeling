67f6176289cfd59baa72f7c396c6f3a9938f0df8
Niels Reijers <nielsreijers@gmail.com>
2013-05-21 12:55:38 +0800

    Changed reprogramming code to allow writing at arbitrary position in the archive. Works for native, still untested for arduino platform.

--- src/lib/wkreprog/c/posix/wkreprog_impl.c
+++ src/lib/wkreprog/c/posix/wkreprog_impl.c
@@ -2,37 +2,35 @@
 #include <strings.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "types.h"
 #include "wkreprog_impl.h"
 
 #include "posix_utils.h"
 
-uint8_t *wkreprog_impl_buffer;
-int wkreprog_impl_pos;
+FILE *fp;
 
 uint16_t wkreprog_impl_get_page_size() {
 	return 256;
 }
 
-bool wkreprog_impl_open(uint16_t size_to_upload) {
-	wkreprog_impl_buffer = malloc(size_to_upload);
-	memset(wkreprog_impl_buffer, 0, size_to_upload);
-	wkreprog_impl_pos = 0;
-	return (wkreprog_impl_buffer != NULL);
+bool wkreprog_impl_open(uint16_t start_write_position) {
+	fp = fopen("app_infusion.dja", "rb+");
+	if (fp== NULL) {
+		printf("Error in opening file to write infusion to...\n");
+	}
+	fseek(fp, start_write_position, SEEK_SET);
+	return true;
 }
 
 void wkreprog_impl_write(uint8_t size, uint8_t* data) {
-	memcpy(wkreprog_impl_buffer+wkreprog_impl_pos, data, size);
-	wkreprog_impl_pos += size;
+	assert(fp != NULL);
+	fwrite(data, size, 1, fp);
 }
 
 void wkreprog_impl_close() {
-  FILE *fp = fopen("app_infusion.dja", "w");
-  if (fp== NULL) {
-	  printf("Error in opening file to write infusion to...\n");
-  }
-  fwrite(wkreprog_impl_buffer, wkreprog_impl_pos, 1, fp);
-  fclose(fp);
+	assert(fp != NULL);
+	fclose(fp);
 }
 
 void wkreprog_impl_reboot() {
