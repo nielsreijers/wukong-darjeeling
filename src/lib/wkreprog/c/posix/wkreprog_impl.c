0c5a30b63278764f7cb516aee440f97dd6583f6c
Niels Reijers <nielsreijers@gmail.com>
2013-05-28 11:17:16 +0800

    Reprogramming for native platform now updates both the on disk archive as well as the data in memory, so that the changes will be visible to the running program.

--- src/lib/wkreprog/c/posix/wkreprog_impl.c
+++ src/lib/wkreprog/c/posix/wkreprog_impl.c
@@ -4,11 +4,22 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "types.h"
+#include "djarchive.h"
 #include "wkreprog_impl.h"
 
 #include "posix_utils.h"
 
+// The posix implementation writes to two places
+// simultaneously.
+//  - fp will be a file handle to app_infusion.dja,
+// positioned at the right location in the file.
+// Writing to fp persists the changes across reboots.
+//  - in_memory_pointer will point to the copy of
+// app_infusion.dja loaded in main memory. Writing to
+// this makes the changes immediately available to the
+// application.
 FILE *fp;
+void *in_memory_pointer;
 
 uint16_t wkreprog_impl_get_page_size() {
 	return 256;
@@ -20,12 +31,17 @@ bool wkreprog_impl_open(uint16_t start_write_position) {
 		printf("Error in opening file to write infusion to...\n");
 	}
 	fseek(fp, start_write_position, SEEK_SET);
+	in_memory_pointer = (void *)di_app_infusion_archive_data + start_write_position;
 	return true;
 }
 
 void wkreprog_impl_write(uint8_t size, uint8_t* data) {
 	assert(fp != NULL);
+	// Write to file
 	fwrite(data, size, 1, fp);
+	// Write to memory
+	memcpy(in_memory_pointer, data, size);
+	in_memory_pointer += size;
 }
 
 void wkreprog_impl_close() {
