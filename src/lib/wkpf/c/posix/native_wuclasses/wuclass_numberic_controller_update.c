17af18e90bb9d6851c5b51a00f70c66e6d189ba5
Niels Reijers <nielsreijers@gmail.com>
2013-06-29 14:38:47 +0200

    Added empty numeric controller update function for native platform.

---
+++ src/lib/wkpf/c/posix/native_wuclasses/wuclass_numberic_controller_update.c
@@ -0,0 +1,12 @@
+#include "debug.h"
+#include "../../common/native_wuclasses/native_wuclasses.h"
+
+#ifdef ENABLE_WUCLASS_NUMERIC_CONTROLLER
+
+void wuclass_numeric_controller_setup(wuobject_t *wuobject) {}
+
+void wuclass_numeric_controller_update(wuobject_t *wuobject) {
+  DEBUG_LOG(DBG_WKPFUPDATE, "WKPFUPDATE(NumericController): NOP\n");
+}
+
+#endif // ENABLE_WUCLASS_NUMERIC_CONTROLLER
