1e5db76ec9f19588184ae955d240656deccb6de4
Penn Su <pennsu@gmail.com>
2013-09-10 12:23:51 +0800

    Will recreate java did after clean up

--- wukong/master/wkpf/wuapplication.py
+++ wukong/master/wkpf/wuapplication.py
@@ -273,11 +273,7 @@ class WuApplication:
 
   def cleanJava(self):
     distutils.dir_util.remove_tree(JAVA_OUTPUT_DIR)
-    try:
-      os.makedirs(JAVA_OUTPUT_DIR)
-    except Exception as e:
-      pass
-
+    os.mkdir(JAVA_OUTPUT_DIR)
 
   def generateJava(self):
       Generator.generate(self.name, self.changesets)
